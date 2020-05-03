019c7e51c799f6f8ceeb4ee39acdff2e0449d111
Sonali <sonalitrivedi1@gmail.com>
2020-05-03 17:12:31 +0530

    Leet day 3 problem

--- Ransom Note.py
+++ Ransom Note.py
@@ -11,7 +11,8 @@ from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         return (Counter(ransomNote) - Counter(magazine)) == {}
-
+    
+# --------------------------------------------------------------------
 #     -------------------------------------------------------------------------
 #   method 2 using the defaultdict
 
